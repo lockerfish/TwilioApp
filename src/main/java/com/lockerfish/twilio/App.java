c08aa4ba1ee0c346fde5781533f8ca7cf5a689ec
Hendrix Tavarez <htavarez@gmail.com>
2018-03-18 18:48:32 -0400

    adding twilio id, token

---
+++ src/main/java/com/lockerfish/twilio/App.java
@@ -0,0 +1,13 @@
+package com.lockerfish.twilio;
+
+/**
+ * HelloWorld
+ */
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Worldaaa");
+		System.out.println(System.getProperty("TWILIO_ACCOUNT_SID"));
+		System.out.println(System.getProperty("TWILIO_AUTH_TOKEN"));
+	}
+}
---
+++ src/test/java/com/lockerfish/twilio/App.java
@@ -0,0 +1,18 @@
+package com.lockerfish.twilio;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * HelloWorld
+ */
+public class App {
+
+	@Test
+	public void HelloLengthIsFive() {
+		assertTrue("Hello".length() == 5);
+
+	}
+
+}
