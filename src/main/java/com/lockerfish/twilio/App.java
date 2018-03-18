319594dfb218c9cac265c5b1d97ee0975a63a7a6
Hendrix Tavarez <htavarez@gmail.com>
2018-03-18 19:46:15 -0400

    sending message

--- src/main/java/com/lockerfish/twilio/App.java
+++ src/main/java/com/lockerfish/twilio/App.java
@@ -1,13 +1,36 @@
 package com.lockerfish.twilio;
 
+import java.util.Scanner;
+
+import com.twilio.Twilio;
+import com.twilio.type.PhoneNumber;
+import com.twilio.rest.api.v2010.account.Message;
+
 /**
  * HelloWorld
  */
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Worldaaa");
-		System.out.println(System.getProperty("TWILIO_ACCOUNT_SID"));
-		System.out.println(System.getProperty("TWILIO_AUTH_TOKEN"));
+
+		String accountSid = System.getProperty("TWILIO_ACCOUNT_SID"); // Your Account SID from www.twilio.com/user/account
+		String authToken = System.getProperty("TWILIO_AUTH_TOKEN"); // Your Auth Token from www.twilio.com/user/account
+		String fromNumber = System.getProperty("TWILIO_OWNED_NUMBER"); // A number owned by this account
+
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Enter number to SMS: ");
+		String toNumber = reader.nextLine();
+
+		System.out.println("Enter message: ");
+		String sms = reader.nextLine();
+
+		Twilio.init(accountSid, authToken);
+
+		Message message = Message.creator(new PhoneNumber(toNumber), // To number
+				new PhoneNumber(fromNumber), // From number
+				sms // SMS body
+		).create();
+
+		System.out.println("message sid: " + message.getSid() + " sent.");
 	}
 }
